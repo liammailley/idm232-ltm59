bb008555b3ff45bd2aebe154123484e2df7a26b6
ltm59 <ltm59@drexel.edu>
2021-12-09 18:36:05 -0500

    Filename working

--- final/admin/recipes/create.php
+++ final/admin/recipes/create.php
@@ -4,18 +4,20 @@ include __DIR__ . '/../../global/header.php';
 // Form has been submitted
 if (isset($_POST['submit'])) {
     //  Parse Data
-    $file_id = mysqli_real_escape_string($db_connection, $_POST['user_id']);
     $recipe_name = mysqli_real_escape_string($db_connection, $_POST['recipe_name']);
     $category = mysqli_real_escape_string($db_connection, $_POST['category']);
     $prep_time = mysqli_real_escape_string($db_connection, $_POST['prep_time']);
     $ingredients = mysqli_real_escape_string($db_connection, $_POST['ingredients']);
     $description = mysqli_real_escape_string($db_connection, $_POST['description']);
+    $file_id = mysqli_real_escape_string($db_connection, $_POST['file_id']);
+    $filename = mysqli_real_escape_string($db_connection, $_POST['filename']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $current_date = getFormattedDateTime();
 
     // Build Query
-    $query = 'INSERT INTO recipes (file_id, recipe_name, category, prep_time, ingredients, description)';
-    $query .= "VALUES ('{$file_id}', '{$recipe_name}', '{$category}', '{$prep_time}', '{$ingredients}', '{$description}')";
+    $query = 'INSERT INTO recipes (recipe_name, category, prep_time, ingredients, description, filename)';
+    $query .= "VALUES ('{$recipe_name}', '{$category}', '{$prep_time}', '{$ingredients}', '{$description}', '{$filename}')";
+    var_dump($query);
 
     // Execute Query
     $db_results = mysqli_query($db_connection, $query);
@@ -31,7 +33,7 @@ if (isset($_POST['submit'])) {
 <div class="container">
   <h1 class="body">Create Recipe</h1>
   <?php include '../../components/alert.php'; ?>
-  <form action="" method="POST" enctype="multipart/form-data">
+  <form action="" method="POST">
 
   <label for="">Recipe Name</label>
     <input type="text"
@@ -62,11 +64,7 @@ if (isset($_POST['submit'])) {
 
   <label for="">Image</label>
     <input type="file"
-      name="image">
-
-    <!--<label for="">Image</label>
-    <input type="file"
-      name="file_id">-->
+      name="filename">
 
     <input type="hidden"
       name="id">
